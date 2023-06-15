4f54af11f49fbed48387989b6a3970809a8fd6e8
lisilya <lis.ilya@gmail.com>
2023-06-15 20:26:14 +0200

    Migrated new category model

--- api/models.py
+++ api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(default='None',max_length=200)
     description = models.TextField()
 
 class Flashcard(models.Model):
     question = models.TextField()
     answer = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
     correct_answers = models.IntegerField(default=0)
     incorrect_answers = models.IntegerField(default=0)
